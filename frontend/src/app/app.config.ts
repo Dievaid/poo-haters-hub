9b15d77acc089cf2ee8654473396bde349cd6870
Luis Micu <miculuis1@gmail.com>
2024-01-13 04:18:12 +0200

    fixed env

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 import { ApiService } from './services/api/api.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
