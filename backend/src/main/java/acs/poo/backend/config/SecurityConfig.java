a1e1e2883b3920f0f08a17bc77cef3d8a7eb84cf
David Capragiu <david.capragiu@gmail.com>
2024-01-13 03:24:07 +0200

    possible cors fix

--- backend/src/main/java/acs/poo/backend/config/SecurityConfig.java
+++ backend/src/main/java/acs/poo/backend/config/SecurityConfig.java
@@ -2,8 +2,6 @@ package acs.poo.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -15,22 +13,10 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of(
-                "https://oop-feed.vercel.app",
-                "https://feed-local.vercel.app"
-        ));
+        corsConfiguration.setAllowedOrigins(List.of("/**"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
         return source;
-    }
-
-    @Bean
-    public SecurityFilterChain setupSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .cors(cors -> cors.configure(httpSecurity))
-                .csrf(csrf -> csrf.configure(httpSecurity))
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .build();
     }
 }
