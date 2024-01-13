668e05281ec271da6dc1256a01ad523e4c52b633
David Capragiu <david.capragiu@gmail.com>
2024-01-13 02:54:46 +0200

    cors explicit

--- backend/src/main/java/acs/poo/backend/config/SecurityConfig.java
+++ backend/src/main/java/acs/poo/backend/config/SecurityConfig.java
@@ -2,12 +2,30 @@ package acs.poo.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://oop-feed.vercel.app",
+                "https://feed-local.vercel.app"
+        ));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
     @Bean
     public SecurityFilterChain setupSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
