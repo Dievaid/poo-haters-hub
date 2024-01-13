6eb77f1c74af9151b9a3f57303f37eb86d02117a
Luis Micu <miculuis1@gmail.com>
2024-01-13 03:01:39 +0200

    added auth locally

---
+++ frontend/src/app/services/api/api.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  // private url = 'http://localhost:9229';
+  private url = 'http://feed-oop.ddns.net:9229';
+
+  constructor(private http: HttpClient) {}
+
+  hasUser(uid: string) {
+    return this.http.get(this.url + '/users');
+  }
+
+  addUser(uid: string, imgURL: string) {
+    const headers = { 'content-type': 'application/json' };
+    const body = JSON.stringify({ uid, imgURL });
+    return this.http.post(this.url + '/users', body, { headers });
+  }
+}
